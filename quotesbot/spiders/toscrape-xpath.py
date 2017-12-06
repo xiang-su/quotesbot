656c2150240e40b7830526457290e0f87d69ed10
xiang-su <271941476@qq.com>
2017-12-06 23:56:02 +0800

    爬取jason博客

--- quotesbot/spiders/toscrape-xpath.py
+++ quotesbot/spiders/toscrape-xpath.py
@@ -5,18 +5,20 @@ import scrapy
 class ToScrapeSpiderXPath(scrapy.Spider):
     name = 'toscrape-xpath'
     start_urls = [
-        'http://quotes.toscrape.com/',
+        'http://atjason.com',
     ]
 
     def parse(self, response):
-        for quote in response.xpath('//div[@class="quote"]'):
+        for atricle in response.xpath('//article[@class="post post-type-normal"]'):
             yield {
-                'text': quote.xpath('./span[@class="text"]/text()').extract_first(),
-                'author': quote.xpath('.//small[@class="author"]/text()').extract_first(),
-                'tags': quote.xpath('.//div[@class="tags"]/a[@class="tag"]/text()').extract()
+                'title': atricle.xpath('.//a[@class="post-title-link"]/text()').extract_first(),
+                'date': atricle.xpath('.//time/text()').extract_first(),
+                'content': atricle.xpath('./div/p/text()').extract()
             }
 
-        next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
+        # next_page_url = response.xpath('//a[@class="page-number"]/@href').extract_first()
+        # if next_page_url is not None:
+        #     yield scrapy.Request(response.urljoin(next_page_url))
+        for num in range(2,167):
+            yield scrapy.Request(response.urljoin('/page/{}'.format(num)))
 
